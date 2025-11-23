da131d1fdc705ca16d69ea73d5d766f0002053c3
RomMLT <bonaw75470@izeao.com>
2025-11-23 04:06:03 +0100

    PART 2

--- hasse.h
+++ hasse.h
@@ -1,6 +1,20 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
 
+#include "utils.h"
+// Structure définie pour les liens
+typedef struct {
+    int from;
+    int to;
+} t_link;
+
+typedef struct {
+    t_link *links;
+    int log_size;    // Nombre de liens
+    int capacity;    // Capacité
+} t_link_array;
+
+
 void removeTransitiveLinks(t_link_array *p_link_array);
 
 /**
@@ -10,5 +24,11 @@ void removeTransitiveLinks(t_link_array *p_link_array);
  * @param graph The adjacency list representation of the graph.
  * @return The created link array.
  */
+
+t_link_array* creer_link_array(int capacite);
+void ajouter_link(t_link_array *array, int from, int to);
+void liberer_link_array(t_link_array *array);
+t_link_array* trouver_liens_entre_classes(t_liste_adjacence *graphe, t_partition *partition);
+void generer_diagramme_hasse_mermaid(t_partition *partition, t_link_array *liens, const char *filename);
 
 #endif
