2c955ecfccac138c0f349bceb29e1ed2d8fc0bfe
Rayen <rayen.bouaouina2@gmail.com>
2025-11-23 04:40:30 +0100

    PART1 FINAL

---
+++ matrix.c
@@ -0,0 +1,5 @@
+//
+// Created by Rayen Bouaouina on 23/11/2025.
+//
+
+#include "matrix.h"
---
+++ matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by Rayen Bouaouina on 23/11/2025.
+//
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#endif //MATRIX_H
